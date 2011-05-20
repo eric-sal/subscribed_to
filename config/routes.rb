d9acbb501ca2cc8b44162a859a74c7d6c7dc05b2
Eric Salczynski <eric@wehaventthetime.com>
2011-05-20 13:37:16 -0400

    MailChimp webhook route must allow GET requests as well in order for MailChimp to initially verify the webhook URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   if SubscribedTo.active
     namespace :subscribed_to do
-      match "mail_chimp" => "mail_chimp_web_hooks#create", :via => :post
+      match "mail_chimp" => "mail_chimp_web_hooks#create", :via => [:get, :post]
     end
   end
 end
