140aac70fd63a697ad8dc76c3fdd15c4c12fc523
Eric Salczynski <eric@wehaventthetime.com>
2011-05-20 13:37:38 -0400

    Bump version to 0.3.2

--- lib/subscribed_to/version.rb
+++ lib/subscribed_to/version.rb
@@ -2,7 +2,7 @@ module SubscribedTo
   module Version  #:nodoc:
     MAJOR = 0
     MINOR = 3
-    PATCH = 1
+    PATCH = 2
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
